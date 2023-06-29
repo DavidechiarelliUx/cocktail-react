8096188b947ebdd52980e74b3ddfdfbae10ddde3
DavidechiarelliUx <chiarellidavide99@gmail.com>
2023-06-29 17:40:23 +0200

    feat:add search bar in app

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,28 +1,57 @@
 import { useState, useEffect } from "react";
-import { GET } from "../../utils/HTTP";
 import ProductItem from "../productItem";
 import "./ProductList.css";
 
 const ProductList = ({ name, setProductSection }) => {
+  const [search, setSearch] = useState('');
   const [listData, setListData] = useState([]);
+  const [errorMsg, setErrorMsg] = useState('');
 
   useEffect(() => {
     fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + name)
       .then((res) => res.json())
       .then((data) => setListData(data.drinks));
-  }, []);
+  }, [name]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (listData.length === 0 || !listData.find(drink => drink.strDrink.toLowerCase() === search.toLowerCase())) {
+      setErrorMsg("I'm sorry but we don't sell this cocktail, try again with one of ours");
+      return;
+    }
+    setErrorMsg('');
+    // handle search here
+  }
 
   return (
-    <div className="ProductList">
-      <h2>{name}</h2>
-      <div className="ProductList__list">
-        {listData.map((drink) => (
-          <ProductItem
-            data={drink}
-            setProductSection={setProductSection}
-            key={drink.idDrink}
+    <div>
+      <div className="SearchCocktails">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder='Search Cocktails'
           />
-        ))}
+          <button type='submit'>Search</button>
+        </form>
+        {errorMsg && <div className='error'>{errorMsg}</div>}
+      </div>
+      <div className="ProductList">
+        <h2>{name}</h2>
+        <div className="ProductList__list">
+          {listData.filter((drink) => {
+            return search.toLowerCase() === ''
+              ? true
+              : drink.strDrink.toLowerCase().includes(search.toLowerCase());
+          }).map((drink) => (
+            <ProductItem
+              data={drink}
+              setProductSection={setProductSection}
+              key={drink.idDrink}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
